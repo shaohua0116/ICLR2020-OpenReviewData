a0f228bc6e38c8538ab1aa261dd62852c0fa88fb
shaohua0116 <waltersun81@gmail.com>
2019-11-07 03:41:23 -0800

    fix crawl data script

--- crawl_data.py
+++ crawl_data.py
@@ -8,7 +8,7 @@ AFTER_DECISION = False
 CRAWL_REVIEW = True
 
 # Get the meta data
-meta_list = crawl_meta(meta_hdf5=None, write_meta_name='data_with_review.hdf5', crawl_review=CRAWL_REVIEW)
+meta_list = crawl_meta(meta_hdf5=None, write_meta_name='data.hdf5', crawl_review=CRAWL_REVIEW)
 num_withdrawn = len([m for m in meta_list if m.withdrawn or m.desk_reject])
 print('Number of submissions: {} (withdrawn/desk reject submissions: {})'.format(
     len(meta_list), num_withdrawn))
